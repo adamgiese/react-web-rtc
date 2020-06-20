abaed9c87a0033eb22eb1a6dd6efa6dd57c29592
Adam Giese <adam.giese@underarmour.com>
2020-06-20 16:54:48 -0500

    Initial react page routing

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,29 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+import Landing from './pages/landing'
+import Join from './pages/join'
+import Host from './pages/host'
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>
+  return <Router>
     <h1>Hello, World</h1>
-  </div>
+    <Switch>
+      <Route path='/host'>
+        <Host />
+      </Route>
+      <Route path='/join'>
+        <Join />
+      </Route>
+      <Route path='/'>
+        <Landing />
+      </Route>
+    </Switch>
+  </Router>
 }
 
 ReactDOM.render(<App />, document.querySelector('#app'));
